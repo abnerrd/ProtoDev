7fedb5ae87494350248ef898c4400124e56c6219
DESKTOP-BDLRKQA\Abner Herrera <abherrera1994@gmail.com>
2017-07-02 00:50:40 -0400

    ~Linked Score to UI. +Added PlayerScore static class. +Added animations to cards for: Intro, Select, Deselect, Vanish. Currently not working properly. ~Changed BTN_OnClick in CardController --> select color change stopped working.

---
+++ ProtoDev/Assets/Scripts/roguelike_tutorial_unity/BoardManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public int _Columns = 8;
+    public int _Rows    = 8;
+
+    private List<Vector3> m_gridPositions = new List<Vector3>();
+
+    void InitializeList()
+    {
+        m_gridPositions.Clear();
+
+        for(int x = 1; x < _Columns - 1; ++x)
+        {
+            for(int y = 1; y < _Rows - 1; ++y)
+            {
+                m_gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
